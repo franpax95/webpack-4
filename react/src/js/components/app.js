db7a39d991d6b46111f208649c624b1d3a12f2ec
Palumbo <fcojnavarrowork@gmail.com>
2020-04-06 18:55:32 +0200

    Soporte de JSX y probando React

---
+++ react/src/js/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return(
+        <div>
+            Qué linda app hecha en ReactJS
+        </div>
+    )
+}
+
+export default App;
---
+++ react/src/js/text.js
@@ -0,0 +1,5 @@
+const text = () => {
+    document.body.innerHTML = '<div><h1>Hola mundo!</h1></div>'
+}
+
+export default text; //ECMAScript6
